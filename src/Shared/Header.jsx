f68a1b5e17f6d1d5c1af7928984058d881dcd662
tahdi-islam <tshdi60@gmail.com>
2022-10-27 00:13:43 +0600

    add dark and light switch

--- src/Shared/Header.jsx
+++ src/Shared/Header.jsx
@@ -90,6 +90,17 @@ export const Header = () => {
                         : <>
                                 <li className='text-gray-200'>{user?.displayName}</li>
                         </>}
+                        <li>
+                            <label htmlFor="Toggle1" className="inline-flex items-center space-x-4 cursor-pointer dark:text-gray-100">
+                                <span>Dark</span>
+                                <span className="relative">
+                                    <input id="Toggle1" type="checkbox" className="hidden peer" />
+                                    <div className="w-10 h-6 rounded-full shadow-inner dark:bg-gray-400 peer-checked:dark:bg-indigo-400"></div>
+                                    <div className="absolute inset-y-0 left-0 w-4 h-4 m-1 rounded-full shadow peer-checked:right-0 peer-checked:left-auto dark:bg-gray-800"></div>
+                                </span>
+                                <span>Light</span>
+                            </label>
+                        </li>
                     </ul>
                     <div className="lg:hidden">
                         <button
