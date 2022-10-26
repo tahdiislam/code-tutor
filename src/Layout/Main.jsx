0c4e9ca8951269a2aa732913b25922a31be8274d
tahdi-islam <tshdi60@gmail.com>
2022-10-26 22:12:27 +0600

    add home page

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import { Footer } from '../Shared/Footer';
 import { Header } from '../Shared/Header';
 
 const Main = () => {
     return (
         <div>
           <Header/>
-          <Outlet/>  
+          <Outlet/>
+          <Footer/>  
         </div>
     );
 };
