35d7f58b6978df1423e9718bdf33f1de492263bc
tahdi-islam <tshdi60@gmail.com>
2022-10-26 20:47:15 +0600

    add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import { RouterProvider } from 'react-router-dom'
 import './App.css'
+import { router } from './Routes/Routes'
 
 function App() {
 
   return (
     <div className="App">
-      
+      <RouterProvider router={router}/>      
     </div>
   )
 }
---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,13 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../Layout/Main";
+import Home from "../Pages/Home";
+
+export const router = createBrowserRouter([
+    {
+        path: '/', element: <Main/>, children: [
+            {
+                path: '/', element: <Home/>
+            },
+        ]
+    }
+])
