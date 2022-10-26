2750bbff7be93a3a2d008bdf21d042ca68c04eed
tahdi-islam <tshdi60@gmail.com>
2022-10-26 22:52:50 +0600

    add register page

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
 import Home from "../Pages/Home";
+import Register from "../Pages/Register";
 import ErrorPage from "../Shared/ErrorPage";
 
 export const router = createBrowserRouter([
@@ -10,6 +11,9 @@ export const router = createBrowserRouter([
             {
                 path: '/', element: <Home/>
             },
+            {
+                path: '/register', element: <Register/>
+            }
         ]
     }
 ])
--- src/Shared/Footer.jsx
+++ src/Shared/Footer.jsx
@@ -199,7 +199,7 @@ export const Footer = () => {
                 </div>
                 <div className="flex flex-col justify-between pt-5 pb-10 border-t border-gray-800 sm:flex-row">
                     <p className="text-sm text-gray-500">
-                        © Copyright 2020 Lorem Inc. All rights reserved.
+                        © Copyright 2022 Code Tutor. All rights reserved.
                     </p>
                     <div className="flex items-center mt-4 space-x-4 sm:mt-0">
                         <a
