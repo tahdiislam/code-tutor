ecb8167be865c7193bd58eefa1284c74e02545bd
tahdi-islam <tshdi60@gmail.com>
2022-10-26 21:44:35 +0600

    add 404 page

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,10 +1,12 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
 import Home from "../Pages/Home";
+import ErrorPage from "../Shared/ErrorPage";
 
 export const router = createBrowserRouter([
     {
         path: '/', element: <Main/>, children: [
+            { path: "*", element: <ErrorPage /> },
             {
                 path: '/', element: <Home/>
             },
