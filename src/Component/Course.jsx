8b683ea4772cd7abdc13083b8b72bdda980f503e
tahdi-islam <tshdi60@gmail.com>
2022-10-27 08:28:56 +0600

    create course card

--- src/Component/Course.jsx
+++ src/Component/Course.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Course = () => {
+const Course = ({course}) => {
+    const {id, title, details, thumbnail, rating} = course;
     return (
         <div>
-            
+            <Link rel="noopener noreferrer" to="/" className="my-4 block max-w-sm gap-3 mx-auto sm:max-w-full group hover:no-underline focus:no-underline lg:grid lg:grid-cols-12 dark:bg-gray-900 hover:shadow-sm hover:shadow-gray-400">
+                <img src={thumbnail} alt="" className="object-cover w-full h-64 rounded sm:h-96 lg:col-span-7 dark:bg-gray-500" />
+                <div className="p-6 space-y-2 lg:col-span-5">
+                    <h3 className="text-gray-200 text-2xl font-semibold sm:text-4xl group-hover:underline group-focus:underline">{title}</h3>
+                    <span className="text-xs dark:text-gray-400">February 19, 2021</span>
+                    <p className='dark:text-gray-400'>{details.length > 300 ? <>{details.slice(0, 300)}...<span className='text-teal-400 hover:underline'>learn more</span></> : <>{details}</>}</p>
+                </div>
+            </Link>
         </div>
     );
 };
--- src/Pages/Courses.jsx
+++ src/Pages/Courses.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import Course from '../Component/Course';
 
 const Courses = () => {
     const courses = useLoaderData()
     return (
-        <div>
-            <h2 className='text-4xl font-semibold text-indigo-400'>Total courses {courses.length}</h2>
-            <div>
-                {
-                    courses.map(course => )
-                }
+        <div className='grid grid-cols-3 gap-4 bg-gray-900 px-4'>
+            <div className='col-span-3 lg:col-span-2'>
+                <h2 className='text-4xl font-semibold text-indigo-400'>Total courses {courses.length}</h2>
+                <div>
+                    {
+                        courses.map(course => <Course key={course.id} course={course} />)
+                    }
+                </div>
+            </div>
+            <div className='lg:col-span-1'>
+                
             </div>
         </div>
     );
