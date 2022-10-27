7142d946aba9eafbbebe9d8c968834598738e988
tahdi-islam <tshdi60@gmail.com>
2022-10-27 11:44:04 +0600

    hide firebase config

--- src/Firebase/firebase.config.jsx
+++ src/Firebase/firebase.config.jsx
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCY5RbmfkYRcsP66Cv_XQXh2yXk0_w2xAU",
-    authDomain: "code-tutor-255e9.firebaseapp.com",
-    projectId: "code-tutor-255e9",
-    storageBucket: "code-tutor-255e9.appspot.com",
-    messagingSenderId: "532250266283",
-    appId: "1:532250266283:web:72c4eca5db48957fed85ee"
+    apiKey: import.meta.env.VITE_apiKey,
+    authDomain: import.meta.env.VITE_authDomain,
+    projectId: import.meta.env.VITE_projectId,
+    storageBucket: import.meta.env.VITE_storageBucket,
+    messagingSenderId: import.meta.env.VITE_messagingSenderId,
+    appId: import.meta.env.VITE_appId
 };
 
 // Initialize Firebase
