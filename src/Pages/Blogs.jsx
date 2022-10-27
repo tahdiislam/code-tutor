cb2a7ba71d53bd4d329a43e111c88d94e7fd3b82
tahdi-islam <tshdi60@gmail.com>
2022-10-27 12:49:35 +0600

    complete blog section

--- src/Pages/Blogs.jsx
+++ src/Pages/Blogs.jsx
@@ -7,7 +7,7 @@ const Blogs = () => {
     return (
         <div className='bg-gray-900'>
             <h2 className='text-center text-4xl font-semibold underline text-indigo-400'>Blog Section</h2>
-            <div>
+            <div className='grid grid-cols-1 md:grid-cols-2 gap-6 lg:mx-8'>
                 {
                     blogs.map(blog => <Blog key={blog.id} blog={blog}/>)
                 }
